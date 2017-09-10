19f4477b9c8b4ea6cb050784407970f248373724
nagabushankampli <nagabushankampli@192.168.0.4>
2017-09-10 16:07:04 +0800

    Updated Main method

---
+++ src/main/java/springDemo/SpringDemoApplication.java
@@ -0,0 +1,17 @@
+package springDemo;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoApplication.class, args);
+		
+
+	}
+}
